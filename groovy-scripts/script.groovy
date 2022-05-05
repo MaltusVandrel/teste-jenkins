8674c64d2dbb2ca4915b1f25d4f347f74fae5dc2
Matheus G. Bento <matheus.bento@eqsengenharia.com.br>
2022-05-05 12:38:30 -0300

    Kinda groovy

---
+++ groovy-scripts/script.groovy
@@ -0,0 +1,11 @@
+def build(){
+    echo 'building in a groovy way...'    
+}
+def test(){
+    echo 'testing in a groovy way...'    
+}
+def deploy(){
+    echo 'deploying in a groovy way...'    
+}
+
+return this
