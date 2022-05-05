97d9dba201492f77f415f2ce86a5c564306dbb24
Matheus G. Bento <matheus.bento@eqsengenharia.com.br>
2022-05-05 13:56:15 -0300

    teste jenkins

--- groovy-scripts/script.groovy
+++ groovy-scripts/script.groovy
@@ -1,13 +1,18 @@
+def test(){
+    echo 'testing in a groovy way...'
+    sh 'ng test'    
+}
 def build(){
     echo 'building in a groovy way...'
-    sh 'pwd'
-    sh 'ls'
-}
-def test(){
-    echo 'testing in a groovy way...'    
+    sh 'ng build --watch'
+    sh 'ng build --watch'
+    
 }
 def deploy(){
-    echo 'deploying in a groovy way...'    
+    echo 'deploying in a groovy way...'
+    sh 'cp -R dist/teste-jenkins /home/arena/applications'    
+    sh 'cp /home/arena/applications/teste-jenkins/assets/logo_.png /home/arena/applications/teste-jenkins/assets/logo.png'    
+    sh 'lite-server --baseDir="/home/arena/applications/teste-jenkins"'    
 }
 
 return this
